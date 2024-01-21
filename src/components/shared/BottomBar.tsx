1e75a6afc368cecfd92c8fc2a474cca565c605e8
Archies13Singh <archiessingh04@gmail.com>
2024-01-21 18:14:12 +0530

    Backend Authentication and Navigations are completed

---
+++ src/components/shared/BottomBar.tsx
@@ -0,0 +1,34 @@
+import { bottombarLinks } from "@/constants";
+import { INavLink } from "@/types";
+import { Link, useLocation } from "react-router-dom";
+
+const BottomBar = () => {
+  const { pathname } = useLocation();
+  return (
+    <div className="bottom-bar">
+      {bottombarLinks.map((link: INavLink) => {
+        const isActive = pathname === link.route;
+        return (
+          <Link
+            key={link.label}
+            className={`${
+              isActive && "bg-primary-500 rounded-[10px]"
+            }  flex-center flex-col gap-1 p-2 transition`}
+            to={link.route}
+          >
+            <img
+              src={link.imgURL}
+              alt={link.label}
+              className={`${isActive && "invert-white"}`}
+              width={20}
+              height={20}
+            />
+            <p className="tiny-large text-light-2">{link.label}</p>
+          </Link>
+        );
+      })}
+    </div>
+  );
+};
+
+export default BottomBar;
---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,83 @@
+import { getCurrentUser } from "@/lib/appWrite/api";
+import { IContextType, IUser } from "@/types";
+import { createContext, useContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+export const INTIAL_USER = {
+  id: "",
+  name: "",
+  username: "",
+  email: "",
+  imageUrl: "",
+  bio: "",
+};
+
+const INTIAL_STATE = {
+  user: INTIAL_USER,
+  isLoading: false,
+  isAuthenticated: false,
+  setUser: () => {},
+  setIsAuthenticated: () => {},
+  checkAuthUser: async () => false as boolean,
+};
+
+const AuthContext = createContext<IContextType>(INTIAL_STATE);
+
+export function AuthProvider({ children }: { children: React.ReactNode }) {
+  const [user, setUser] = useState<IUser>(INTIAL_USER);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const navigate = useNavigate();
+  const checkAuthUser = async () => {
+    setIsLoading(true);
+    try {
+      const currentAccount = await getCurrentUser();
+      if (currentAccount) {
+        setUser({
+          id: currentAccount.$id,
+          name: currentAccount.name,
+          username: currentAccount.username,
+          email: currentAccount.email,
+          imageUrl: currentAccount.imageUrl,
+          bio: currentAccount.bio,
+        });
+
+        setIsAuthenticated(true);
+        return true;
+      }
+      return false;
+    } catch (error) {
+      console.error("User is Not Authenticated : ", error);
+      return false;
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    const cookieFallback = localStorage.getItem("cookieFallback");
+    if (
+      cookieFallback === "[]" ||
+      cookieFallback === null || 
+      cookieFallback === undefined
+    ) {
+      navigate("/sign-in");
+    }
+
+    checkAuthUser();
+  }, []);
+
+  const value = {
+    user,
+    setUser,
+    isLoading,
+    isAuthenticated,
+    setIsAuthenticated,
+    checkAuthUser,
+  };
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+}
+
+export default AuthContext;
+
+export const useUserContext = () => useContext(AuthContext);
